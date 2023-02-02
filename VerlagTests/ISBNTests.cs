e964c0ac840900fde335be169801b5fe912734d2
Aaron Suszka <aaron.suszka@gmail.com>
2023-02-02 07:58:06 +0100

    ISBN Tests in eigene Klasse verschoben.

---
+++ VerlagTests/ISBNTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Verlag;
+
+namespace VerlagTests
+{
+    [TestClass]
+    public class ISBNTests
+    {
+        [TestMethod]
+        public void ISBN_KannEingegebenWerden()
+        {
+            // Arrange 
+            string autor = "Autor";
+            string titel = "Titel";
+            int auflage = 1;
+            string iSBN = "978-3770436149";
+
+            // Act
+            Buch c = new Buch(autor, titel, auflage, iSBN);
+
+            // Assert
+            Assert.AreEqual(iSBN, c.ISBN);
+        }
+
+        [TestMethod]
+        public void ISBN_PruefzifferWirdRichtigBerechnet()
+        {
+            // Arrange
+            string autor = "Autor";
+            string titel = "Titel";
+            int auflage = 1;
+            string iSBN = "978-377043614";
+
+            // Act
+            Buch d = new Buch(autor, titel, auflage, iSBN);
+
+            // Assert
+            Assert.AreEqual("978-3770436149", d.ISBN);
+        }
+
+        [TestMethod]
+        public void ISBN_ISBN13inISBN10Umrechnen()
+        {
+            //Arrange 
+            string autor = "Autor";
+            string titel = "Titel";
+            int auflage = 1;
+            string iSBN = "978-3770436064";
+            Buch d = new Buch(autor, titel, auflage, iSBN);
+
+            // Act
+            string iSBN10 = d.ISBN10;
+
+            // Assert
+            Assert.AreEqual("3770436067", iSBN10);
+        }
+    }
+}
